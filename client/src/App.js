99b811a1829a2a435350436fe9277b82c114d863
Graham <gtompkins1993@gmail.com>
2021-11-28 17:30:12 -0500

    SecondBranch Commit

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">Another change</div>;
+  return (
+    <>
+      <div className="App">Still on Master branch</div>
+      <div className="App">Still on Master branch</div>
+      <div>Now I am on SecondBranch</div>
+      <div>Now I am on SecondBranch</div>
+    </>
+  );
 }
 
 export default App;
