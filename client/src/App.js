52e2ee0b9b0258b217a55d862458bf7d8ca7929c
Graham <gtompkins1993@gmail.com>
2021-11-28 17:42:41 -0500

    test from other Window

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,16 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">Another change</div>;
+  return (
+    <>
+      {" "}
+      <span>
+        Testing to see if this causes a conflict. I put this in before switching
+        to a new branch
+      </span>
+      <span>Test after switching to a new branch</span>
+    </>
+  );
 }
 
 export default App;
