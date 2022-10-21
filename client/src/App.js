cc9dee01c32c21efca08bcdc4bd0fd9baf0f046f
Graham Tompkins <84285032+gtomp93@users.noreply.github.com>
2022-10-20 22:03:42 -0400

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       {" "}
-      <span>Going</span>
+      <span>Editing from GitHub</span>
       <span>Test after switching to a new branch</span>
       <div className="App">Still on Master branch</div>
       <div>Now I am on SecondBranch</div>
